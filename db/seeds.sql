e2cc96c711cea0df8e9e3fd3aae64cc749e00d7e
Jake Colledge <jakecolledgework@gmail.com>
2021-11-24 00:06:34 +1030

    added some object to database

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+-- adds data to tables
+INSERT INTO department (id, name)
+VALUES (1, "Salon"),
+       (2, "Retail");
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (1, 'Hair Stylist', 23.67, 1),
+       (2, 'Shelf Stocker', 29.32, 2);
+
+INSERT INTO employee (id, first_name, last_name, role_id)
+VALUES (1, 'Jake', 'Colledge', 1),
+       (2, 'Eva', 'Pelotta', 2);
